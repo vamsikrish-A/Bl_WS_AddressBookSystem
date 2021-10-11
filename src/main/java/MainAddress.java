1b0259c432dce71d53deabacc364f6765e01ae98
vamsikrish-A <krishnavamsi165@gmail.com>
2021-10-11 08:32:36 +0530

    Welcome message

---
+++ src/main/java/MainAddress.java
@@ -0,0 +1,12 @@
+public class MainAddress {
+    /*
+    * @purpose: AddressBookSystem workshop.
+    *
+    * @Author: Vamsi Krishna A
+    *
+    * @Date: 11-10-2021.
+    * */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book WorkShop");
+    }
+}
